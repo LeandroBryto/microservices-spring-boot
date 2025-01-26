2e763bea369e75fa35c5d1389b6072bc188e2f9a
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-01-26 17:06:32 -0300

    Implementa logging
    mplementa logging no serviço de usuários.
    Adiciona logs de sucesso e erro no processo de salvar e enviar email.
    Loga criação e envio de email de usuário.
    Adiciona tratamento de exceções e logs para envio de email.
    "Inclui log de salvamento de usuário e envio de email

--- user/src/main/java/com/leandrodev/ms/controller/UserController.java
+++ user/src/main/java/com/leandrodev/ms/controller/UserController.java
@@ -3,7 +3,13 @@ package com.leandrodev.ms.controller;
 import com.leandrodev.ms.dtos.UserRecordDto;
 import com.leandrodev.ms.models.UserModel;
 import com.leandrodev.ms.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,16 +18,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "User ", tags = "User")
 @RestController
- class UserController {
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserService userService;
 
+    @ApiOperation(value = "Create a new user", notes = "This endpoint creates a new user with the provided details.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User successfully created"),
+            @ApiResponse(code = 400, message = "Invalid input data"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @PostMapping("/users")
-    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+        logger.info("Iniciando requisição para criar usuário com e-mail: {}", userRecordDto.email());
+
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto , userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
+
+        try {
+            logger.info("Salvando usuário: {}", userRecordDto.email());
+            UserModel savedUser = userService.save(userModel);
+            logger.info("Usuário com e-mail {} criado com sucesso!", userRecordDto.email());
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        } catch (Exception e) {
+            logger.error("Erro ao salvar o usuário com e-mail {}: {}", userRecordDto.email(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
--- user/src/main/java/com/leandrodev/ms/service/UserService.java
+++ user/src/main/java/com/leandrodev/ms/service/UserService.java
@@ -1,14 +1,19 @@
 package com.leandrodev.ms.service;
 
+
 import com.leandrodev.ms.models.UserModel;
 import com.leandrodev.ms.producers.UserProducer;
 import com.leandrodev.ms.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     UserProducer userProducer;
@@ -17,8 +22,18 @@ public class UserService {
 
     @Transactional
     public UserModel save(UserModel userModel){
+        logger.info("Iniciando o processo de salvar o usuário: {}", userModel.getName());
+
         userModel = userRepository.save(userModel);
-        userProducer.publisMessageEmail(userModel);
+        logger.info("Usuário salvo com sucesso. ID: {}", userModel.getUserId());
+
+        try {
+            userProducer.publisMessageEmail(userModel);
+            logger.info("Mensagem de email enviada para o usuário: {}", userModel.getEmail());
+        } catch (Exception e) {
+            logger.error("Erro ao enviar email para o usuário: {}", userModel.getEmail(), e);
+        }
+
         return userModel;
     }
 
