ee042da9aaa023d42f865ef9d049613b07c2d209
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-01-26 16:41:00 -0300

    validação
    Adiciona UserRecordDto com validação de nome e e-mail

---
+++ user/src/main/java/com/leandrodev/ms/dtos/UserRecordDto.java
@@ -0,0 +1,7 @@
+package com.leandrodev.ms.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRecordDto(@NotBlank String name , @NotBlank @Email String email) {
+}
