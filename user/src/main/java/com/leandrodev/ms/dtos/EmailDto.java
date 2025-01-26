d272f42bb47c47b25cff5b070087a60ae80bd127
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-01-26 16:40:05 -0300

    Adiciona EmailDto
    Adiciona EmailDto para transferência de dados de e-mail

---
+++ user/src/main/java/com/leandrodev/ms/dtos/EmailDto.java
@@ -0,0 +1,42 @@
+package com.leandrodev.ms.dtos;
+
+import java.util.UUID;
+
+public class EmailDto {
+    private UUID userId;
+    private String emailTo;
+    private String subject;
+    private String text;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailTo(String email) {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
